5578fdb16dd83a89756b7baa523fe824abb4de40
nullmodder <nullmodder@gmail.com>
2026-04-25 17:12:00 -0500

    Actualizar ClientTab.lua

--- ClientTab.lua
+++ ClientTab.lua
@@ -204,7 +204,7 @@ RunService.Heartbeat:Connect(function()
         
         if MoveDirection.Magnitude > 0 then
           HumanoidRootPart.CFrame = CFrame.lookAt(HumanoidRootPart.Position, CameraLookVector)
-          HumanoidRootPart.CFrame = HumanoidRootPart.CFrame + Vector3.new(HumanoidRootPartLookVector * FloatSpeed)
+          HumanoidRootPart.CFrame = HumanoidRootPart.CFrame + (HumanoidRootPart.Position + HumanoidRootPartLookVector * FloatSpeed)
           Humanoid.PlatformStand = true
           Workspace.Gravity = 0
         end
